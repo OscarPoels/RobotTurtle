8a4756a2c8d1b883b60e9055b04a68c3b811d193
Oscar Poels <o.poels@gmail.com>
2020-01-08 23:46:33 +0100

    Changement du chemin d'une image

--- src/Graphisme/Background.java
+++ src/Graphisme/Background.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Background extends JPanel {
     public void paintComponent(Graphics g) {
         try {
-            Image img = ImageIO.read(new File("C:\\Users\\Oscar\\IdeaProjects\\untilted\\src\\com\\images\\background.jpeg"));
+            Image img = ImageIO.read(new File("images\\background.jpeg"));
             //g.drawImage(img, 0, 0, this);
             //Pour une image de fond
             g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
